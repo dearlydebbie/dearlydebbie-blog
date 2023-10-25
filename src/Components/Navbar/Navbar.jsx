2783a813123e4aad8442b5b484f697998742a8ec
Deborah Adeoye <debbieadeoye1@gmail.com>
2023-10-25 10:07:00 +0100

    responsive burger menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -7,23 +7,29 @@ import AuthLinks from "../AuthLinks/AuthLinks";
 
 const Navbar = () => {
   return (
-  <div className={styles.container}>
-    <div className={styles.social}>
-      <Image src="/facebook.png" alt="facebook" width={24} height={24} />
-      <Image src="/instagram.png" alt="instagram" width={24} height={24} />
-      <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
-      <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/instagram.png" alt="instagram" width={24} height={24} />
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>DearlyDebbie</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <Link href="/" className={styles.link}>
+          Homepage
+        </Link>
+        <Link href="/" className={styles.link}>
+          Contact
+        </Link>
+        <Link href="/" className={styles.link}>
+          About
+        </Link>
+        <AuthLinks />
+      </div>
     </div>
-    <div className={styles.logo}>DearlyDebbie</div>
-    <div className={styles.links}>
-      <ThemeToggle />
-      <Link href="/">Homepage</Link>
-      <Link href="/">About</Link>
-      <Link href="/">Contact</Link>
-      <AuthLinks />
-    </div>
-  </div>
-  )
+  );
 };
 
-export default Navbar
+export default Navbar;
